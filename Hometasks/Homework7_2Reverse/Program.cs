ce99b9a7dd15cb23f9306221974ea2a4e515e564
Yana Khaustova <khaustovayana91@gmail.com>
2022-11-22 14:16:31 +0200

    Add Hometsk7_2ReverseArray

---
+++ Hometasks/Homework7_2Reverse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+static int[] MyReverse(int[] array)
+{
+    int N = 0;
+    Random r = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = r.Next(10, 200);
+
+    }
+
+
+    int[] reversedArray = new int[array.Length];
+    int j = 0;
+    for (int i = reversedArray.Length - 1; i >= 0; i--)
+    {
+        reversedArray[j++] = array[i];
+    }
+    return reversedArray;
+
+}
+
+Console.WriteLine("Enter number");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+int[] reversedArray = MyReverse(array);
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"{array[i]}");
+}
+Console.WriteLine();
+for (int i = 0; i < reversedArray.Length; i++)
+{
+    Console.WriteLine($"{reversedArray[i]}");
+}
